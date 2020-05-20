9e8880cc32e0abb7b6f42ed01540ee8f1e2cc54a
muhammadasadbastomi <tomycules@gmail.com>
2020-05-20 10:26:54 +0800

    make migration kota

---
+++ database/migrations/2020_05_19_212713_create_kotas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKotasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kotas', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid', 36);
+            $table->string('nama_kota', 25);
+            $table->tinyInteger('zona');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kotas');
+    }
+}
