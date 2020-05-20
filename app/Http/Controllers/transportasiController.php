f4da2f90f1f48844d946a9163395a58e5e445d19
muhammadasadbastomi <tomycules@gmail.com>
2020-05-20 10:34:26 +0800

    skeleton crud transportasi

--- app/Http/Controllers/transportasiController.php
+++ app/Http/Controllers/transportasiController.php
@@ -2,17 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Transportasi;
 use Illuminate\Http\Request;
 
 class transportasiController extends Controller
 {
-    public function index(){
-        
-        return view('admin.transportasi.index');
+    public function index()
+    {
+        $data = Transportasi::orderBy('id', 'desc')->get();
+        return view('admin.transportasi.index', compact('data'));
     }
 
-    public function edit(){
-        
-        return view('admin.transportasi.edit');
+    public function store(Request $request)
+    {
+        $data = new transportasi;
+        // $data->kode_transportasi = $request->kode_transportasi;
+        // $data->transportasi = $request->transportasi;
+
+        $data->save();
+
+        return redirect()->route('transportasiIndex')->with('success', 'Data berhasil disimpan');
+    }
+
+    public function edit($uuid)
+    {
+        $data = transportasi::where('uuid', $uuid)->first();
+        return view('admin.transportasi.edit', compact('data'));
+    }
+
+    public function update(Request $request, $uuid)
+    {
+        $data = transportasi::where('uuid', $uuid)->first();
+        // $data->kode_transportasi = $request->kode_transportasi;
+        // $data->transportasi = $request->transportasi;
+
+        $data->update();
+
+        return redirect()->route('transportasiIndex')->with('success', 'Data berhasil diubah');
+    }
+
+    public function destroy($uuid)
+    {
+        $data = transportasi::where('uuid', $uuid)->first()->delete();
+        return redirect()->route('transportasiIndex');
     }
 }
