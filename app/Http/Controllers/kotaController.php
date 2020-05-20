1d8b571e55b9fd43e68a0a35d6408764c578dede
muhammadasadbastomi <tomycules@gmail.com>
2020-05-20 10:26:26 +0800

    crud kota controller

--- app/Http/Controllers/kotaController.php
+++ app/Http/Controllers/kotaController.php
@@ -2,17 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Kota;
 use Illuminate\Http\Request;
 
 class kotaController extends Controller
 {
-    public function index(){
-        
-        return view('admin.kota.index');
+    public function index()
+    {
+        $data = Kota::orderBy('id', 'desc')->get();
+        return view('admin.kota.index', compact('data'));
     }
 
-    public function edit(){
-        
-        return view('admin.kota.edit');
+    public function store(Request $request)
+    {
+        $data = new kota;
+        $data->nama_kota = $request->nama_kota;
+        $data->zona = $request->zona;
+
+        $data->save();
+
+        return redirect()->route('kotaIndex')->with('success', 'Data berhasil disimpan');
+    }
+
+    public function edit($uuid)
+    {
+        $data = kota::where('uuid', $uuid)->first();
+        return view('admin.kota.edit', compact('data'));
+    }
+
+    public function update(Request $request, $uuid)
+    {
+        $data = kota::where('uuid', $uuid)->first();
+        $data->nama_kota = $request->nama_kota;
+        $data->zona = $request->zona;
+
+        $data->update();
+
+        return redirect()->route('kotaIndex')->with('success', 'Data berhasil diubah');
+    }
+
+    public function destroy($uuid)
+    {
+        $data = kota::where('uuid', $uuid)->first()->delete();
+        return redirect()->route('kotaIndex');
     }
 }
