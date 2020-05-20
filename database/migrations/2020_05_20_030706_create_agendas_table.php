0f8a9c2e09d3013b88439030967b29ecce4b843d
muhammadasadbastomi <tomycules@gmail.com>
2020-05-20 11:15:25 +0800

    create migration agenda table

---
+++ database/migrations/2020_05_20_030706_create_agendas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agendas', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid', 36);
+            $table->string('no_agenda', 30);
+            $table->string('keterangan', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agendas');
+    }
+}
