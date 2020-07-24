d8bb80f0713ef4196a01b8edb9fc826eb9458ba5
muhammadasadbastomi <tomycules@gmail.com>
2020-07-24 11:51:53 +0800

    crud function pembatalan sppd

--- app/Http/Controllers/pembatalanSPPDController.php
+++ app/Http/Controllers/pembatalanSPPDController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Pegawai;
+use App\Pembatalan_sppd;
 use App\Sppd;
 use Illuminate\Http\Request;
 
@@ -12,13 +13,44 @@ class pembatalanSPPDController extends Controller
     {
         $sppd = Sppd::latest()->get();
         $pegawai = Pegawai::latest()->get();
-        return view('admin.pembatalanSPPD.index',compact('sppd','pegawai'));
+        $data = Pembatalan_sppd::whereHas('sppd', function ($d) {
+            $d->where('status', 0);
+        })->get();
+        return view('admin.pembatalanSPPD.index', compact('sppd', 'pegawai', 'data'));
+    }
+
+    public function store(Request $req)
+    {
+        $data = Pembatalan_sppd::create($req->all());
+        $sppd = Sppd::findOrFail($req->sppd_id);
+        $sppd->status = 0;
+        $sppd->update();
+
+        return back()->withSuccess('Data berhasil disimpan');
     }
 
     public function edit($uuid)
     {
+        $data = Pembatalan_sppd::where('uuid', $uuid)->first();
+
         $sppd = Sppd::latest()->get();
         $pegawai = Pegawai::latest()->get();
-        return view('admin.pembatalanSPPD.edit',compact('sppd','pegawai'));
+        return view('admin.pembatalanSPPD.edit', compact('sppd', 'pegawai', 'data'));
+    }
+
+    public function update(Request $req, $uuid)
+    {
+        $data = Pembatalan_sppd::where('uuid', $uuid)->first();
+
+        $data->fill($req->all())->save();
+
+        return redirect()->route('pembatalanSPPDIndex')->withSuccess('Data berhasil diubah');
+    }
+
+    public function destroy($uuid)
+    {
+        $data = Pembatalan_sppd::where('uuid', $uuid)->first()->delete();
+
+        return back()->withSuccess('Data berhasil dihapus');
     }
 }
