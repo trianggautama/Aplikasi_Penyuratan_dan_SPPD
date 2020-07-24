b26310436cd39318ca5bc7ac20040ff86fac69c8
muhammadasadbastomi <tomycules@gmail.com>
2020-07-24 11:51:42 +0800

    crud function pembatalan rincian sppd

--- app/Http/Controllers/pembatalanRincianSPPDController.php
+++ app/Http/Controllers/pembatalanRincianSPPDController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Pegawai;
+use App\Pembatalan_rincian_sppd;
 use App\Rincian_sppd;
 use App\Sppd;
 use Illuminate\Http\Request;
@@ -11,24 +12,51 @@ class pembatalanRincianSPPDController extends Controller
 {
     public function index()
     {
-        $sppd = Sppd::latest()->get();
-        return view('admin.pembatalanRincianSPPD.index',compact('sppd'));
+        $sppd = Sppd::where('status', 1)->latest()->get();
+        return view('admin.pembatalanRincianSPPD.index', compact('sppd'));
     }
 
     public function show($uuid)
     {
-        $sppd        = Sppd::where('uuid',$uuid)->first();
-        $rincianSppd = Rincian_sppd::latest()->get();
-        $pegawai     = Pegawai::latest()->get();
-        return view('admin.pembatalanRincianSPPD.show',compact('sppd','rincianSppd','pegawai'));
+        $sppd = Sppd::where('uuid', $uuid)->first();
+        $rincianSppd = Rincian_sppd::where('sppd_id', $sppd->id)->latest()->get();
+        $pegawai = Pegawai::latest()->get();
+        return view('admin.pembatalanRincianSPPD.show', compact('sppd', 'rincianSppd', 'pegawai'));
+    }
+
+    public function store(Request $req)
+    {
+        $data = Pembatalan_rincian_sppd::create($req->all());
+        $rincianSppd = Rincian_sppd::findOrFail($req->rincian_sppd_id);
+        $rincianSppd->status = 0;
+        $rincianSppd->update();
+
+        return back()->withSuccess('Data berhasil disimpan');
     }
 
     public function edit($uuid)
     {
-        $sppd        = Sppd::where('uuid',$uuid)->first();
+        $data = Pembatalan_rincian_sppd::where('uuid', $uuid)->first();
         $rincianSppd = Rincian_sppd::latest()->get();
-        $pegawai     = Pegawai::latest()->get();
-        return view('admin.pembatalanRincianSPPD.edit',compact('sppd','rincianSppd','pegawai'));
+        $pegawai = Pegawai::latest()->get();
+        return view('admin.pembatalanRincianSPPD.edit', compact('data', 'rincianSppd', 'pegawai'));
+    }
+
+    public function update(Request $req, $uuid)
+    {
+        $data = Pembatalan_rincian_sppd::where('uuid', $uuid)->first();
+
+        $data->fill($req->all())->save();
+        $data->update();
+
+        return redirect()->route('pembatalanRincianSPPDShow', ['uuid' => $data->sppd->uuid])->withSuccess('Data berhasil diubah');
+    }
+
+    public function destroy($uuid)
+    {
+        $data = Pembatalan_rincian_sppd::where('uuid', $uuid)->first()->delete();
+
+        return back()->withSuccess('Data berhasil dihapus');
     }
 
 }
