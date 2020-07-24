4bde769cb8c188c4d6e959d4c92523e63aba8ee8
muhammadasadbastomi <tomycules@gmail.com>
2020-07-24 11:49:49 +0800

    add relation into kategoris table

--- app/Anggaran_detail.php
+++ app/Anggaran_detail.php
@@ -23,4 +23,29 @@ class Anggaran_detail extends Model
         return $this->belongsTo(Rincian_sppd::class);
     }
 
+    public function harian()
+    {
+        return $this->belongsTo('App\Kategori', 'harian_id');
+    }
+
+    public function representasi()
+    {
+        return $this->belongsTo('App\Kategori', 'representasi_id');
+    }
+
+    public function penginapan()
+    {
+        return $this->belongsTo('App\Kategori', 'penginapan_id');
+    }
+
+    public function tiket()
+    {
+        return $this->belongsTo('App\Kategori', 'tiket_id');
+    }
+
+    public function taksi()
+    {
+        return $this->belongsTo('App\Kategori', 'taksi_id');
+    }
+
 }
